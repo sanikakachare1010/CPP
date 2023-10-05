d61957af6e8b5d7a70938baac65fc10d2d62a618
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-10-05 11:20:14 -0700

    Basic

---
+++ Basic Program/Addition of Two Numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int No1 = 0, No2 = 0,Sum = 0;
+
+    cout<<"\n Enter 1st Number : ";
+    cin>>No1;
+    cout<<"\n Enter 2nd Number : ";
+    cin>>No2;
+
+    Sum = No1 + No2 ;
+
+    cout<<endl<<"Addition of "<<No1<<" and "<<No2<<" is "<<Sum;
+
+    getch();
+    return 0;
+
+}
