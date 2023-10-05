f7ac188675e0c71d2fe3ce37611e03e59d2f0f93
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-10-05 11:25:23 -0700

    Basic

---
+++ Basic Program/Substraction of Two Numbers.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int No1 = 0, No2 = 0,Sub = 0;
+
+    cout<<"\n Enter 1st Number : ";
+    cin>>No1;
+    cout<<"\n Enter 2nd Number : ";
+    cin>>No2;
+
+    Sub = No1 - No2 ;
+
+    cout<<endl<<"Substraction of "<<No1<<" and "<<No2<<" is "<<Sum;
+
+    getch();
+    return 0;
+
+}
