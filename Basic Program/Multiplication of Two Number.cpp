dfe807c4d7eda3b09b35809235872526d99aef32
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-10-06 11:22:02 -0700

    Basic

---
+++ Basic Program/Multiplication of Two Number.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int No1 = 0, No2 = 0,Mul = 0;
+
+    cout<<"\n Enter 1st Number : ";
+    cin>>No1;
+    cout<<"\n Enter 2nd Number : ";
+    cin>>No2;
+
+    Mul = No1 * No2 ;
+
+    cout<<endl<<"Multiplicatin  of "<<No1<<" and "<<No2<<" is "<<Mul;
+
+    getch();
+    return 0;
+
+}
+
