049fcf9ae2654b5c24b6419bf1d22a90cbebd3f5
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-11-30 12:11:19 -0500

    Bitwise Operater

--- Bitwise Operaters/Bitwise Operater.cpp
+++ Bitwise Operaters/Bitwise Operater.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int iNo1 =13;
+    int iNo2 = 24;
+
+    int iAns = 0;
+
+    iAns = iNo1 & iNo2;
+    cout<<"Bitwise and : "<<iAns<<"\n";
+
+     iAns = iNo1 | iNo2;
+    cout<<"Bitwise Or : "<<iAns<<"\n";
+
+     iAns = iNo1 ^ iNo2;
+    cout<<"Bitwise XOr : "<<iAns<<"\n";
+    
+
+    return 0;
+}
